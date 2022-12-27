89b25f03789f461ab08cd46ba9776a4d32ecc2c8
Vaibhav <Vaibhavojha2002@gmail.com>
2022-12-27 20:54:28 +0530

    commit on 'main'

--- Postgram/settings.py
+++ Postgram/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = 'django-insecure-a7+l!j$p-k6r+kot@x6y@-sygjd4ya1^%!zlk)9)@j%r+80@96
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
-ALLOWED_HOSTS = ['localhost','127.0.0.1','.vercel.app','.now.sh']
-=======
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.vercel.app','.now.sh','*']
+
 # Application definition
 
 INSTALLED_APPS = [
